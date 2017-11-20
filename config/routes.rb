2ec205bfbae6706b75ea63cf8dac87a8f8fa28e2
Grigorios Tantanozis <tarzanbros@gmail.com>
2017-11-20 15:55:22 +0100

    4.3 Added controlers and views for future pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
+
+  get 'simple_pages/landing_page'
+  root 'simple_pages#landing_page'
 
   get 'simple_pages/index'
   root 'simple_pages#index'
