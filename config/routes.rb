a885d892357298b7e8428937eeb840d8d0d1154f
Grigorios Tantanozis <tarzanbros@gmail.com>
2017-11-24 01:22:11 +0100

    deleted second root

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/landing_page'
-  root 'simple_pages#landing_page'
 
   get 'simple_pages/index'
   root 'simple_pages#index'
