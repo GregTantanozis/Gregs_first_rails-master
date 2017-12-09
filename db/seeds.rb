7bed81eb89b1c840102c8f362f71d9e2352ff073
Grigorios Tantanozis <tarzanbros@gmail.com>
2017-12-09 19:36:20 +0100

    postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Apartheid Of Thoughts", description: "EP", image_url: "album cover.jpg", colour: "Reggae", price: '4', href: "https://tarzanbros.bandcamp.com/album/apartheid-of-thoughts")
+Product.create(name: "Apartheid Of Thoughts", description: "EP", image_url: "album cover.jpg", genre: "Reggae", price: '4', href: "https://tarzanbros.bandcamp.com/album/apartheid-of-thoughts")
 
-Product.create(name: "Chess With Your Life", description: "EP", image_url: "chess.jpg", colour: "Reggae", price: '4', href: "https://tarzanbros.bandcamp.com/album/chess-with-your-life")
+Product.create(name: "Chess With Your Life", description: "EP", image_url: "chess.jpg", genre: "Reggae", price: '4', href: "https://tarzanbros.bandcamp.com/album/chess-with-your-life")
 
-Product.create(name: "Reborn", description: "Single", image_url: "reborn.jpg", colour: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/reborn")
+Product.create(name: "Reborn", description: "Single", image_url: "reborn.jpg", genre: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/reborn")
 
-Product.create(name: "Imperial Bros", description: "Single", image_url: "imperial.jpg", colour: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/album/imperial-bros")
+Product.create(name: "Imperial Bros", description: "Single", image_url: "imperial.jpg", genre: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/album/imperial-bros")
 
-Product.create(name: "Malecon", description: "Single", image_url: "malecon.jpg", colour: "Cumbia", price: '1', href: "https://tarzanbros.bandcamp.com/track/oh-malecon")
+Product.create(name: "Malecon", description: "Single", image_url: "malecon.jpg", genre: "Cumbia", price: '1', href: "https://tarzanbros.bandcamp.com/track/oh-malecon")
 
-Product.create(name: "Zealots", description: "Cover (Fugees)", image_url: "zealots.jpg", colour: "Reggae", price: '0', href: "https://tarzanbros.bandcamp.com/track/fugees-zealots-tarzan-bros-re-imagined")
+Product.create(name: "Zealots", description: "Cover (Fugees)", image_url: "zealots.jpg", genre: "Reggae", price: '0', href: "https://tarzanbros.bandcamp.com/track/fugees-zealots-tarzan-bros-re-imagined")
 
-Product.create(name: "Jerry", description: "Remix", image_url: "jerry.jpg", colour: "Reggae-Classic Jazz", price: '1', href: "https://tarzanbros.bandcamp.com/track/jerry-tarzanbros-remix")
+Product.create(name: "Jerry", description: "Remix", image_url: "jerry.jpg", genre: "Reggae-Classic Jazz", price: '1', href: "https://tarzanbros.bandcamp.com/track/jerry-tarzanbros-remix")
 
-Product.create(name: "Tomato", description: "Remix", image_url: "tomato.jpg", colour: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/dont-touch-me-tomato-phyllis-dillon-tarzan-bros-remix")
+Product.create(name: "Tomato", description: "Remix", image_url: "tomato.jpg", genre: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/dont-touch-me-tomato-phyllis-dillon-tarzan-bros-remix")
 
-Product.create(name: "Skanville", description: "Remix", image_url: "skanville.jpg", colour: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/ethiopians-train-to-skaville-tarzanbros-remix")
+Product.create(name: "Skanville", description: "Remix", image_url: "skanville.jpg", genre: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/ethiopians-train-to-skaville-tarzanbros-remix")
 
-Product.create(name: "B - Hop - Diggin", description: "Remix", image_url: "diggin.jpg", colour: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/b-hop-diggin")
+Product.create(name: "B - Hop - Diggin", description: "Remix", image_url: "diggin.jpg", genre: "Reggae", price: '1', href: "https://tarzanbros.bandcamp.com/track/b-hop-diggin")
